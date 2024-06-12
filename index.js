cbbff79efbff7781464df39274aac42ac9a9dfe6
Admin <lmht721@gmail.com>
2024-06-12 16:54:26 +0700

    update

--- index.js
+++ index.js
@@ -2,46 +2,30 @@ var btnCheck = document.getElementById("btn1");
 btnCheck.addEventListener("click", getCheck);
 function getCheck() {
   let month = document.getElementById("select1").value;
-  // let message = '';
+  let message = "";
   switch (month) {
-    case document.getElementById("option1").value:
-      document.getElementById("result").innerHTML = "Tháng 1 có 31 ngày";
+    case "Tháng 1":
+    case "Tháng 3":
+    case "Tháng 5":
+    case "Tháng 7":
+    case "Tháng 8":
+    case "Tháng 10":
+    case "Tháng 12":
+      message = month + " có 31 ngày";
       break;
-    case document.getElementById("option2").value:
-      document.getElementById("result").innerHTML = "Tháng 2 có 30 ngày";
+    case "Tháng 4":
+    case "Tháng 6":
+    case "Tháng 9":
+    case "Tháng 11":
+      message = month + " có 30 ngày";
       break;
-    case document.getElementById("option3").value:
-      document.getElementById("result").innerHTML = "Tháng 3 có 31 ngày";
-      break;
-    case document.getElementById("option4").value:
-      document.getElementById("result").innerHTML = "Tháng 4 có 30 ngày";
-      break;
-    case document.getElementById("option5").value:
-      document.getElementById("result").innerHTML = "Tháng 5 có 31 ngày";
-      break;
-    case document.getElementById("option6").value:
-      document.getElementById("result").innerHTML = "Tháng 6 có 30 ngày";
-      break;
-    case document.getElementById("option7").value:
-      document.getElementById("result").innerHTML = "Tháng 7 có 31 ngày";
-      break;
-    case document.getElementById("option8").value:
-      document.getElementById("result").innerHTML = "Tháng 8 có 31 ngày";
-      break;
-    case document.getElementById("option9").value:
-      document.getElementById("result").innerHTML = "Tháng 9 có 30 ngày";
-      break;
-    case document.getElementById("option10").value:
-      document.getElementById("result").innerHTML = "Tháng 10 có 31 ngày";
-      break;
-    case document.getElementById("option11").value:
-      document.getElementById("result").innerHTML = "Tháng 11 có 30 ngày";
-      break;
-    case document.getElementById("option12").value:
-      document.getElementById("result").innerHTML = "Tháng 12 có 31 ngày";
+    case "Tháng 2":
+      message = month + " có 28 hoặc 29 ngày";
       break;
     default:
       document.getElementById("result").innerHTML =
         "Vui lòng chọn số tháng phù hợp";
   }
+
+  document.getElementById("result").innerHTML = message;
 }
